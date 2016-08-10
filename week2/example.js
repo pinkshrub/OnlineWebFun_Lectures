d79b09d6e0c1d2838bdb2bd3fa0155bb4f44310e
Todd Enders <tenders64@gmail.com>
2016-08-10 14:55:47 -0700

    jQuery part I

---
+++ week2/example.js
@@ -0,0 +1,5 @@
+var arr1 = [1,2,3,4,5];
+// [2,3,4,5,5]
+// arr1.length = arr1.length - 1;
+arr1[arr1.length] = 6;
+console.log(arr1);
---
+++ week2/officehours1.md
@@ -0,0 +1,5 @@
+#Web Fundamentals - Week 2
+####Office Hours
+- Your Q&A
+- CSS
+- Yellow Belt
